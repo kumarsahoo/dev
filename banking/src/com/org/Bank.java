0c503fe6cd31270d77f473aa85f49458984a04b3
sanjay <sanjay@ideapoke.com>
2019-08-20 17:46:09 +0530

    ggg

---
+++ banking/src/com/org/Bank.java
@@ -0,0 +1,12 @@
+package com.org;
+
+public class Bank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		getAccount();
+	}
+public static void getAccount(){
+	System.out.println("1000");
+}
+}
